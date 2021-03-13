5db33803e50bf1ab536f80ad815998a4090fb664
Lava84flow <Lava84flow@users.noreply.github.com>
2021-03-12 17:18:31 -0800

    Update customers.js

--- public/customers.js
+++ public/customers.js
@@ -14,3 +14,20 @@ function getAddresses(customerId) {
       }
     });
 }
+
+function getOrders(customerId) {
+  let classIdURL =
+    "/getOrders?id=" + customerId;
+  fetch(classIdURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+      if (jsObject.length == 0) {
+        console.log("NO RESULTS");
+        document.getElementById("output").innerHTML = "NO RESULTS";
+      } else {
+		let out = JSON.stringify(jsObject);
+        console.log(out);
+        document.getElementById("output").innerHTML = out;
+      }
+    });
+}
