6fbfcd5a3047ef64780bec20275ee8a7325750e9
Lava84flow <Lava84flow@users.noreply.github.com>
2021-03-13 16:20:33 -0800

    Update customers.js

--- public/customers.js
+++ public/customers.js
@@ -51,9 +51,9 @@ function getOrders(customerId) {
       } else {
         var x = '';
         let data = jsObject;
-        console.log(data);
+        //console.log(data);
         for (i = 0; i < data.length; i++) {
-          x += '<tr> <td>' + data[i].address_type + '</td> <td>' + data[i].address_line1 + '</td> <td>' + data[i].address_line2 + '</td> <td>' + data[i].city + '</td> <td>' + data[i].state + '</td> <td>' + data[i].zipcode + '</td> </tr>';
+          x += '<tr> <td>' + data[i].first_name + '</td> <td>' + data[i].last_name + '</td> <td>' + data[i].subtotal + '</td> <td>' + data[i].taxes + '</td> <td>' + data[i].shipping + '</td> <td>' + data[i].status + '</td> <td>' + data[i].shipping_address + '</td> <td>' + data[i].billing_address + '</td> </tr>';
         }
 		    let out = '<table style="width: 100%; color: black;"> <tr> <th>First Name</th> <th>Last Name</th> <th>Subtotal</th> <th>Taxes</th> <th>Shipping</th> <th>Status</th> <th>Shipping Address</th> <th>Billing Address</th> </tr>' + x + '</table>';
 
