6284a555c9ac72444725340f4b2a88234665b475
Lava84flow <Lava84flow@users.noreply.github.com>
2021-04-03 20:48:39 -0700

    adding password reset form

--- index.js
+++ index.js
@@ -96,6 +96,10 @@ app.get('/past-orders', (req, res) => {
   })
 })
 
+app.get('/reset-password', (req, res) => {
+  res.render('pages/reset-password'
+})
+
   app.get('/getAddresses', getAddresses);
 
   app.get('/getOrders', getOrders);
